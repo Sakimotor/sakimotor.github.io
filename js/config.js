729cc2a6204a7f677ded79e23e3910f7ee6f46ed
Sakimotor <mambila2001@gmail.com>
2019-02-16 11:40:48 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var l_serverImage = "";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "[FR/EN] Calamity Deathrun";
+var l_serverName = "[FR/EN] Kusabi Deathrun";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
